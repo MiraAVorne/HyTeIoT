7345b34f891e2c2fad676a12136b705382df1dc4
MiraAVorne <mira@vorne.org>
2021-03-23 07:41:57 +0000

    20210323

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Chart from 'react-google-charts';
 
 function App() {
+  const initWeather = []; //esimerkiksi mahdollisia tulevia haun filtteröintejä varten tarvitaan state. Esim. näytetäänkö hakutuloksista vain tasatunnein mitatut
+  const [weather, setWeather] = useState(initWeather);
+
+  function convertUTCDateToLocalDate(date) {
+    new Date(date.getTime() + date.getTimezoneOffset()*60*1000);
+    return date;
+  }
+
+  let chartHumData = [
+    ['Aika', '%'],
+    ['Please wait...', 0]
+  ];
+  let chartTempData = [
+    ['Aika', 'Asteet'],
+    ['Please wait...', 0]
+  ];
+
+  fetch('https://iot-opettaja.azurewebsites.net/api/HttpTriggerCSharp2?code=mDniSJtbOvdFMvOqapzzY506azTIbraWxtejgzhX8Ti4sY2p/kjoDg==&deviceId=1f0038001847393035313138&amount=10')
+    .then(response => response.json())
+    .then(json => setWeather([...json]));
+
+  let humtempkey = 1;
+  
+  const rows = () => weather.map(temphum => { //loopataan läpi uusimmat 10kpl (amount=10) mittaustuloksia
+
+    if(chartHumData[1][0] === 'Please wait...'){
+      chartHumData.pop();
+    }
+    if(chartTempData[1][0] === 'Please wait...'){
+      chartTempData.pop();
+    }
+
+    chartHumData.push([String(convertUTCDateToLocalDate(new Date(temphum.Timestamp))).split(' ')[4], parseInt(temphum.Hum)])
+    chartTempData.push([String(convertUTCDateToLocalDate(new Date(temphum.Timestamp))).split(' ')[4], parseInt(temphum.Temp)])
+
+    return <div key={humtempkey++}>
+      <b>Klo:</b> {String(convertUTCDateToLocalDate(new Date(temphum.Timestamp))).split(' ')[4]} &nbsp;
+      <b>Ilmankosteus:</b> {temphum.Hum} % &nbsp;
+      <b>Lämpötila:</b> {temphum.Temp} °C 
+    </div>
+  }) 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {rows()}
+      <div>
+        <Chart
+          width={1000}
+          height={300}
+          chartType="ColumnChart"
+          loader={<div>Loading Chart</div>}
+          data={chartHumData}
+          options={{
+            title: 'Ilmankosteus',
+            hAxis: {
+              minValue: 0,
+            }, 
+          }}
+        />
+        </div>
+        <div>
+        <Chart
+          width={1000}
+          height={300}
+          chartType="LineChart"
+          loader={<div>Loading Chart</div>}
+          data={chartTempData}
+          options={{
+            title: 'Lämpötila',
+            vAxis: { minValue: 0 },
+          }}
+        />
+      </div>
     </div>
   );
 }
-
 export default App;
