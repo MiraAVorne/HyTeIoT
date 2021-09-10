9a36ec05021e41b7ea220afbbf79b2cacc1d0a57
MiraAVorne <61496264+MiraAVorne@users.noreply.github.com>
2021-09-10 10:58:33 +0000

    20200910

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import React, { useState } from 'react';
 
 function Portfolio() {
+
+  const initPulse = []; 
+  const [pulse, setPulse] = useState(initPulse);
+
+  fetch('https://iot-opettaja.azurewebsites.net/api/HttpTriggerCSharp2?code=mDniSJtbOvdFMvOqapzzY506azTIbraWxtejgzhX8Ti4sY2p/kjoDg==&deviceId=1f0038001847393035313138&amount=10')
+    .then(response => response.json())
+    .then(json => setPulse([...json]));
+
     return (
       <div>
         Kirjoita tänne sähköinen CV sekä tietoa opiskeluprojekteistasi tulevia työnantajia varten :)
